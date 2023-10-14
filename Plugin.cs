c4db6f977a6dabee073fb1ef1df96ffd6b6a8b6a
Георгий Асатрян <ago-2004@mail.ru>
2023-10-14 13:48:44 +0300

    Use [HarmonyPatch] attribute on plugin class, instead of making separate patch class

--- Plugin.cs
+++ Plugin.cs
@@ -6,6 +6,7 @@ namespace PeglinCore
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     [BepInProcess("Peglin.exe")]
+    [HarmonyPatch]
     public class Plugin : BaseUnityPlugin
     {
         private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
@@ -15,11 +16,10 @@ namespace PeglinCore
             harmony.PatchAll();
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
-    }
 
-    [HarmonyPatch(typeof(PeglinUI.LoadoutManager.LoadoutManager), "SetupDataForNewGame")]
-    class AchievementsPatch {
-        static private void Postfix() {
+        [HarmonyPatch(typeof(PeglinUI.LoadoutManager.LoadoutManager), "SetupDataForNewGame")]
+        [HarmonyPostfix]
+        static private void PatchSetupDataForNewGame() {
             AchievementManager.AchievementsOn = false;
         }
     }
